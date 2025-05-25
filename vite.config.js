1c67cc4ce44e44c6bd86417afff9ae75c598618a
milanmatka <milanmatka777@gmail.com>
2025-05-25 17:24:27 +0000

    s

--- vite.config.js
+++ vite.config.js
@@ -3,33 +3,45 @@ import react from '@vitejs/plugin-react';
 import { resolve } from 'path';
 import fs from 'fs/promises';
 import svgr from '@svgr/rollup';
+// import svgr from 'vite-plugin-svgr'
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  resolve: {
-    alias: {
-      src: resolve(__dirname, 'src'),
-    },
-  },
-  esbuild: {
-    loader: 'jsx',
-    include: /src\/.*\.jsx?$/,
-    exclude: [],
-  },
-  optimizeDeps: {
-    esbuildOptions: {
-      plugins: [
-        {
-          name: 'load-js-files-as-jsx',
-          setup(build) {
-            build.onLoad({ filter: /src\\.*\.js$/ }, async (args) => ({
-              loader: 'jsx',
-              contents: await fs.readFile(args.path, 'utf8'),
-            }));
-          },
+    resolve: {
+        alias: {
+            src: resolve(__dirname, 'src'),
         },
-      ],
     },
-  },
-  plugins: [svgr(), react()],
-  // base: '/', // <-- Default is fine for Vercel, or just remove this line
+    esbuild: {
+        loader: 'jsx',
+        include: /src\/.*\.jsx?$/,
+        exclude: [],
+    },
+    optimizeDeps: {
+        esbuildOptions: {
+            plugins: [
+                {
+                    name: 'load-js-files-as-jsx',
+                    setup(build) {
+                        build.onLoad(
+                            { filter: /src\\.*\.js$/ },
+                            async (args) => ({
+                                loader: 'jsx',
+                                contents: await fs.readFile(args.path, 'utf8'),
+                            })
+                        );
+                    },
+                },
+            ],
+        },
+    },
+
+
+    
+    // plugins: [react(),svgr({
+    //   exportAsDefault: true
+    // })],
+
+    plugins: [svgr(), react()],
+    base: '/Modernize-Vite'
 });
